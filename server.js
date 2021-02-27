af0f5eb368f74e6f93878b316d9e90f6d189e454
Watthanai <watthanai2540@gmail.com>
2021-02-27 21:55:23 +0700

    update commit

---
+++ server.js
@@ -0,0 +1,34 @@
+let http = require('http');
+let parse =require('url-parse') ;
+let fs = require('fs');
+const express = requrie("express");
+const app = express();
+const port = 8000;
+// const port = process.env.port ||8000
+
+
+
+// http.createServer(function(req,res){
+
+//     let q =parse(req.url,true);
+//     let filename = "."+q.pathname;
+
+//     console.log(filename);
+//     fs.readFile(filename,function(err,data){
+//     if (err){     
+//     res.writeHead(404,{'Content-Type':'text/html'});
+//     return res.end('404 Not Found');
+
+      
+//     }
+//     res.writeHead(200,{'Content-Type':'text/html'});
+//     res.write(data);
+//     return res.end();
+
+//    })
+// })
+
+app.get('/',(req,res) => res.send('Hello World'));   
+
+
+app.listen(8000,()=>console.log("Seerver is running..."));
