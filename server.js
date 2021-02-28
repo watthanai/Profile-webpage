2c4481c31bb75db0546e0c3b69131be226d000ae
Watthanai <watthanai2540@gmail.com>
2021-02-28 11:59:17 +0700

    Update 2

--- server.js
+++ server.js
@@ -30,6 +30,18 @@ const app = express();
 
 app.get('/',(req,res) => {
     res.send('I Love Peach very much')
+    let q =parse(req.url,true);
+    let filename = "."+q.pathname;
+    console.log(filename);
+    fs.readFile(filename,function(err,data){
+    if (err){     
+    res.writeHead(404,{'Content-Type':'text/html'});
+    return res.end('404 Not Found');
+    
+    }
+    res.writeHead(200,{'Content-Type':'text/html'});
+    res.write(data);
+    return res.end();
 
 });   
 
