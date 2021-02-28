82759b5f0957e924174dd17ee1d187c20b5de340
Watthanai <watthanai2540@gmail.com>
2021-02-28 13:17:42 +0700

    html

--- server.js
+++ server.js
@@ -1,38 +1,20 @@
-let http = require('http');
-let parse =require('url-parse') ;
-let fs = require('fs');
 const express = require('express');
 const app = express();
-// const port = 8000;
- const port = process.env.PORT ||8000
+const port = process.env.PORT ||8000
+const path = require('path');
+const { allowedNodeEnvironmentFlags } = require('process');
 
 
-
-// http.createServer(function(req,res){
-
-//     let q =parse(req.url,true);
-//     let filename = "."+q.pathname;
-
-//     console.log(filename);
-//     fs.readFile(filename,function(err,data){
-//     if (err){     
-//     res.writeHead(404,{'Content-Type':'text/html'});
-//     return res.end('404 Not Found');
-
-      
-//     }
-//     res.writeHead(200,{'Content-Type':'text/html'});
-//     res.write(data);
-//     return res.end();
-
-//    })
-// })
-
-app.get('/',(req,res) => {
-    res.send('I Love Peach very much')
+// app.get('/',(req,res) => {
+//     res.sendFile(path.join(__dirname,'Publish','index.html'));
  
-});   
+// });   
 
 
-app.listen(port,()=>{
-    console.log("Seerver is running...")});
+//Set static folder
+app.use(express.static(path.join(__dirname,'Publish')))
+
+
+
+app.listen(port,()=> console.log('Server is running ' +port)
+);
